69420181a6665605bc0b54dcfad1657acb01e170
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-01-20 18:55:48 -0600

    Added documentation to get specific playerID endpoint.

--- src/app.ts
+++ src/app.ts
@@ -14,13 +14,21 @@ app.get("/api/v1/players", (req, res) => {
     res.json(data)
 });
 
+/**
+ * Gets a player's details by their ID.
+ * @returns 200 - Returns the player object.
+ * @returns 404 - Returns an error message if it doesn't exist.
+ */
 app.get("/api/v1/players/:id", (req, res) => {
+    //parses player ID from string to number
     const playerID = parseInt(req.params.id);
+    //calls the function passing the ID as a param
     const player = getPlayerWithId(playerID);
     
     if (player) {
         res.json(player);
     } else {
+
         res.status(404).json({
             message: `Cannot find player ${playerID}`
         });
